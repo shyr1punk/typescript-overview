2b950ff496b25307eecb266be88ba3f0936df1c9
Alexander Sivashev <sivashev@yandex-team.ru>
2018-10-17 10:12:49 +0300

    Delete interfaces.js

--- interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getCarMaxSpeed(car) {
-    return car.color.trim();
-}
-const add = (x, y) => {
-    return x + y;
-};
